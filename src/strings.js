09c8c1452021eb1c09d5d5cd22ba8e77fd6b9b70
Edvin Svenblad <edvin.svenblad@regeringen.ax>
2025-06-19 14:33:22 +0300

    disclaimer

--- src/strings.js
+++ src/strings.js
@@ -19,11 +19,11 @@ const STRINGS = {
 	upper_limits_heading 	: { sv:"Övre gränser:", en:"Upper limits:", fi:"Ylärajat:" },
 
 	// Disclaimer (only shown if non‐empty)
-	disclaimer: {
-		sv: "",
-		en: "⚠  Unverified Machine translated text",
-		fi: "⚠ Tämä teksti on konekäännetty eikä sitä ole tarkistanut ihminen."
-	},
+        disclaimer: {
+                sv: "⚠ Du ansvarar själv för de beräknade resultaten.",
+                en: "⚠ Unverified machine translated text. You are responsible for the calculated results.",
+                fi: "⚠ Tämä teksti on konekäännetty eikä sitä ole tarkistanut ihminen. Käyttäjä on vastuussa laskettujen tulosten paikkansapitävyydestä."
+        },
 
 	// Language selector (labels not strictly necessary since flags are obvious)
 	language_label: {
